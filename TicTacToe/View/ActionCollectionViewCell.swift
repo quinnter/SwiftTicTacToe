f0ecd4f4c46233d5c10c3b236b16caa7ebf1dc80
quinnter <lauraaquinn16@gmail.com>
2019-08-08 14:57:03 +0100

    set up action view cell

--- TicTacToe/View/ActionCollectionViewCell.swift
+++ TicTacToe/View/ActionCollectionViewCell.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class ActionCollectionViewCell: UICollectionViewCell {
     let actionView = ActionView(frame: CGRect.zero)
+    
+    var action = Action() {
+        didSet {
+            actionView.action = action
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(actionView)
+        actionView.translatesAutoresizingMaskIntoConstraints = false
+        actionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        actionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        actionView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        actionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("\(#function) has not been implemented.")
+    }
 }
